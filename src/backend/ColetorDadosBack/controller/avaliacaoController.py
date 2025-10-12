933ce266693ab2b70b00ef26208a3cebc4aa2746
Rafael <rafaelfariafranco30@gmail.com>
2025-10-12 02:03:07 -0300

    Feat-Adiciona controllers para favoritos e avaliacao

---
+++ src/backend/ColetorDadosBack/controller/avaliacaoController.py
@@ -0,0 +1,65 @@
+from sqlalchemy.orm import Session
+from ..dto import avaliacaoDTO;
+from ..service.avaliacaoService import AvaliacaoService;
+from fastapi import APIRouter, Depends, status, HTTPException;
+from ..config.database import get_db;
+
+router = APIRouter(
+    prefix="/api/avaliacoes",
+    tags=["Avaliacoes"]
+)
+
+
+@router.get('/get-all', response_model=list[avaliacaoDTO.AvaliacaoResponse], status_code=status.HTTP_200_OK)
+def get_all(
+    db: Session = Depends(get_db)
+):
+    service = AvaliacaoService(db=db);
+    return service.get_all();
+
+@router.get('/get/{imdbID}', response_model=avaliacaoDTO.AvaliacaoResponse, status_code=status.HTTP_200_OK)
+def get_by_imdbID(
+        imdbID: int,
+        db: Session = Depends(get_db)
+):
+    service = AvaliacaoService(db=db);
+    avaliacao = service.get_by_imdbID(imdbID);
+    if not avaliacao:
+        raise HTTPException(status_code=404, detail=f"Avaliacao para o filme com imdbID: {imdbID} não encontrado");
+
+    return avaliacao;
+
+@router.delete('/remove/{imdbID}', status_code=status.HTTP_200_OK)
+def remove_avaliacao(
+    imdbID: str,
+    db: Session = Depends(get_db)
+):
+    service = AvaliacaoService(db=db);
+    deletado = service.remove(imdbID);
+    if deletado == None:
+        raise HTTPException(status_code=404, detail=f"Filme com id {imdbID} não encontrado");
+
+    return HTTPException(status_code=204, detail="Deletado com sucesso.");
+
+@router.post('/add', response_model=avaliacaoDTO.AvaliacaoResponse, status_code=status.HTTP_201_CREATED)
+def add_avaliacao(
+    avaliacao_request : avaliacaoDTO.AvaliacaoRequest,
+    db : Session = Depends(get_db)
+):
+    service = AvaliacaoService(db=db);
+    nova_avaliacao = service.add(avaliacao_request);
+    if not nova_avaliacao:
+        raise HTTPException(status_code=400, detail="Filme já avaliado.");
+    return nova_avaliacao;
+
+@router.put('/update', response_model=avaliacaoDTO.AvaliacaoResponse, status_code=status.HTTP_200_OK)
+def update_avaliacao(
+        avaliacao_request : avaliacaoDTO.AvaliacaoRequest,
+        db : Session = Depends(get_db),
+):
+    service = AvaliacaoService(db=db);
+    nova_avaliacao = service.update(avaliacao_request);
+    if not nova_avaliacao:
+        raise HTTPException(status_code=409, detail="Não foi possível alterar avaliacao.");
+
+    return nova_avaliacao;
---
+++ src/backend/ColetorDadosBack/controller/favoritoController.py
@@ -0,0 +1,41 @@
+from sqlalchemy.orm import Session
+from ..dto import favoritoDTO;
+from ..service.favoritosService import FavoritosService;
+from fastapi import APIRouter, Depends, status, HTTPException;
+from ..config.database import get_db;
+
+router = APIRouter(
+    prefix="/api/favoritos",
+    tags=["Favoritos"]
+)
+
+
+@router.get('/get-all', response_model=list[favoritoDTO.FavoritosResponse], status_code=status.HTTP_200_OK)
+def get_all(
+    db: Session = Depends(get_db)
+):
+    service = FavoritosService(db=db);
+    return service.get_all();
+
+@router.delete('/remove/{imdbID}', status_code=status.HTTP_200_OK)
+def remove_favorito(
+    imdbID: str,
+    db: Session = Depends(get_db)
+):
+    service = FavoritosService(db=db);
+    deletado = service.remove_favorito(imdbID);
+    if deletado == None:
+        raise HTTPException(status_code=404, detail=f"Filme com id {imdbID} não encontrado");
+
+    return HTTPException(status_code=204, detail="Deletado com sucesso.");
+
+@router.post('/add', response_model=favoritoDTO.FavoritosResponse, status_code=status.HTTP_201_CREATED)
+def add_favorito(
+    favorito_request: favoritoDTO.FavoritosRequest,
+    db: Session = Depends(get_db)
+):
+    service = FavoritosService(db=db);
+    novo_favorito = service.add_favorito(favorito_request)
+    if not novo_favorito:
+        raise HTTPException(status_code=400, detail="Filme já existe nos favoritos.");
+    return novo_favorito;
