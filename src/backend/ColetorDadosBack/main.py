a5c23d45868761f8c62ddace179f9c2a751c25e5
Rafael <rafaelfariafranco30@gmail.com>
2025-10-12 01:58:41 -0300

    Feat-Adiciona arquivo main para conectar todas as controllers

--- src/backend/ColetorDadosBack/main.py
+++ src/backend/ColetorDadosBack/main.py
@@ -1,1 +1,22 @@
-import os
+from fastapi import FastAPI
+
+from .controller import favoritoController, avaliacaoController
+from .config.database import engine, Base
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    title="API Coletor de Dados de Filmes",
+    description="API para o desafio técnico da dti digital, desenvolvida com FastAPI.",
+    version="0.1.0"
+)
+
+print("Incluindo o router de favoritos...")
+app.include_router(favoritoController.router)
+app.include_router(avaliacaoController.router)
+# app.include_router(historicoController.router)
+
+
+@app.get("/")
+def read_root():
+    return {"status": "API no ar!", "documentacao": "/docs"}
