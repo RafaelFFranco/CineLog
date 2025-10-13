d616219501093571d9469b4ff2468b060d840b88
Rafael <rafaelfariafranco30@gmail.com>
2025-10-12 21:23:20 -0300

    Adjust-Adiciona blocos try-catch para validação de retorno

--- src/backend/ColetorDadosBack/controller/avaliacaoController.py
+++ src/backend/ColetorDadosBack/controller/avaliacaoController.py
@@ -14,52 +14,67 @@ router = APIRouter(
 def get_all(
     db: Session = Depends(get_db)
 ):
-    service = AvaliacaoService(db=db);
-    return service.get_all();
+    try:
+        service = AvaliacaoService(db=db);
+        return service.get_all();
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e));
 
 @router.get('/get/{imdbID}', response_model=avaliacaoDTO.AvaliacaoResponse, status_code=status.HTTP_200_OK)
 def get_by_imdbID(
         imdbID: int,
         db: Session = Depends(get_db)
 ):
-    service = AvaliacaoService(db=db);
-    avaliacao = service.get_by_imdbID(imdbID);
-    if not avaliacao:
-        raise HTTPException(status_code=404, detail=f"Avaliacao para o filme com imdbID: {imdbID} não encontrado");
+    try:
+        service = AvaliacaoService(db=db);
+        avaliacao = service.get_by_imdbID(imdbID);
+        if not avaliacao:
+            raise HTTPException(status_code=404, detail=f"Avaliacao para o filme com imdbID: {imdbID} não encontrado");
 
-    return avaliacao;
+        return avaliacao;
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e));
 
 @router.delete('/remove/{imdbID}', status_code=status.HTTP_200_OK)
 def remove_avaliacao(
     imdbID: str,
     db: Session = Depends(get_db)
 ):
-    service = AvaliacaoService(db=db);
-    deletado = service.remove(imdbID);
-    if deletado == None:
-        raise HTTPException(status_code=404, detail=f"Filme com id {imdbID} não encontrado");
+    try:
+        service = AvaliacaoService(db=db);
+        deletado = service.remove(imdbID);
+        if deletado == None:
+            raise HTTPException(status_code=404, detail=f"Filme com id {imdbID} não encontrado");
 
-    return HTTPException(status_code=204, detail="Deletado com sucesso.");
+        return HTTPException(status_code=204, detail="Deletado com sucesso.");
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e));
 
 @router.post('/add', response_model=avaliacaoDTO.AvaliacaoResponse, status_code=status.HTTP_201_CREATED)
 def add_avaliacao(
     avaliacao_request : avaliacaoDTO.AvaliacaoRequest,
     db : Session = Depends(get_db)
 ):
-    service = AvaliacaoService(db=db);
-    nova_avaliacao = service.add(avaliacao_request);
-    if not nova_avaliacao:
-        raise HTTPException(status_code=400, detail="Filme já avaliado.");
-    return nova_avaliacao;
+    try:
+        service = AvaliacaoService(db=db);
+        nova_avaliacao = service.add(avaliacao_request);
+        if not nova_avaliacao:
+            raise HTTPException(status_code=400, detail="Filme já avaliado.");
+        return nova_avaliacao;
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e));
 
 @router.put('/update', response_model=avaliacaoDTO.AvaliacaoResponse, status_code=status.HTTP_200_OK)
 def update_avaliacao(
         avaliacao_request : avaliacaoDTO.AvaliacaoRequest,
         db : Session = Depends(get_db),
 ):
-    service = AvaliacaoService(db=db);
-    nova_avaliacao = service.update(avaliacao_request);
-    if not nova_avaliacao:
-        raise HTTPException(status_code=409, detail="Não foi possível alterar avaliacao.");
+    try:
+        service = AvaliacaoService(db=db);
+        nova_avaliacao = service.update(avaliacao_request);
+        if not nova_avaliacao:
+            raise HTTPException(status_code=409, detail="Não foi possível alterar avaliacao.");
 
-    return nova_avaliacao;
+        return nova_avaliacao;
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e));
--- src/backend/ColetorDadosBack/controller/favoritoController.py
+++ src/backend/ColetorDadosBack/controller/favoritoController.py
@@ -14,28 +14,37 @@ router = APIRouter(
 def get_all(
     db: Session = Depends(get_db)
 ):
-    service = FavoritosService(db=db);
-    return service.get_all();
+    try:
+        service = FavoritosService(db=db);
+        return service.get_all();
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e));
 
 @router.delete('/remove/{imdbID}', status_code=status.HTTP_200_OK)
 def remove_favorito(
     imdbID: str,
     db: Session = Depends(get_db)
 ):
-    service = FavoritosService(db=db);
-    deletado = service.remove_favorito(imdbID);
-    if deletado == None:
-        raise HTTPException(status_code=404, detail=f"Filme com id {imdbID} não encontrado");
+    try:
+        service = FavoritosService(db=db);
+        deletado = service.remove_favorito(imdbID);
+        if deletado == None:
+            raise HTTPException(status_code=404, detail=f"Filme com id {imdbID} não encontrado");
 
-    return HTTPException(status_code=204, detail="Deletado com sucesso.");
+        return HTTPException(status_code=204, detail="Deletado com sucesso.");
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Erro ao remover favorito." + str(e));
 
 @router.post('/add', response_model=favoritoDTO.FavoritosResponse, status_code=status.HTTP_201_CREATED)
 def add_favorito(
     favorito_request: favoritoDTO.FavoritosRequest,
     db: Session = Depends(get_db)
 ):
-    service = FavoritosService(db=db);
-    novo_favorito = service.add_favorito(favorito_request)
-    if not novo_favorito:
-        raise HTTPException(status_code=400, detail="Filme já existe nos favoritos.");
-    return novo_favorito;
+    try:
+        service = FavoritosService(db=db);
+        novo_favorito = service.add_favorito(favorito_request)
+        if not novo_favorito:
+            raise HTTPException(status_code=400, detail="Filme já existe nos favoritos.");
+        return novo_favorito;
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e));
