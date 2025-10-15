75423035b11a0cd223acf43fcc581635faf6c840
Rafael <rafaelfariafranco30@gmail.com>
2025-10-15 20:34:44 -0300

    Config-Adiciona configurações de cors

--- src/backend/ColetorDadosBack/main.py
+++ src/backend/ColetorDadosBack/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from .controller import favoritoController, avaliacaoController, historicoController
 from .config.database import engine, Base
@@ -10,6 +11,21 @@ app = FastAPI(
     description="API para o desafio técnico da dti digital, desenvolvida com FastAPI.",
     version="0.1.0"
 )
+
+origins=[
+    "http://localhost",
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,      # Especifica as origens
+    allow_credentials=True,
+    allow_methods=["*"],        # Permite todos os métodos Http
+    allow_headers=["*"],        # Permite todos os cabeçalhos
+)
+
+
 
 print("Incluindo o router de favoritos...")
 app.include_router(favoritoController.router)
